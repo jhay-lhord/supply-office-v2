847491982d93a18e870d210016b1a7ef0e13a9d9
Crislyn2002 <crislyn.tanudtanud@gmail.com>
2024-11-18 13:19:37 +0800

    PITI GWAPA

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ const HomePage = () => {
       <div style={styles.content}>
         <h1 style={styles.homeText}>Home</h1>
         <h1>Supply Office Management System</h1>
+        <p>PITI</p>
       </div>
     </div>
   );
